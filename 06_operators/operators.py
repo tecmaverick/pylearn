e399753e3ea40ba9d4b6128b0c0770b4e726b268
Peter <abrpeter@amazon.com>
2018-01-21 18:24:34 +1100

    addded new dir 06_oeprators

---
+++ 06_operators/operators.py
@@ -0,0 +1,9 @@
+fno = 3.5
+sno = 7.5
+
+result = sno / fno
+resultIntDivision = sno // fno
+
+print "Division {} / {} result:{}".format(sno,fno,result)
+print "Integer division {} // {} result:{}".format(sno,fno,resultIntDivision)
+
