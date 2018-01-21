3fbfa2640992dbccd0203fc9fbe92d8e6948ae57
Peter <abrpeter@amazon.com>
2018-01-21 18:18:22 +1100

    added new dir 05_strings

---
+++ 05_strings/stringdemo.py
@@ -0,0 +1,10 @@
+import math
+
+val = math.factorial(5)
+
+#Convert to string using str() and get the length of the string using len() function
+print "String length of factorial {} is {}".format(5,len(str(val)))
+
+
+someval = "AlphaBeta"
+print ("String length of val {} is {}".format(someval,len(someval)))
