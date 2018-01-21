9f49865d9b55774f9e8f998bb04d02fd185cc41c
Peter <abrpeter@amazon.com>
2018-01-21 20:22:24 +1100

    added dir 08_confitionalexp

---
+++ 08_ConditionalExp/conditionalexp.py
@@ -0,0 +1,29 @@
+if True:
+	print "yes"
+
+if False:
+	print "no"
+
+
+if bool("help"):
+	print "please help"
+
+if "someval":
+	print "yes string contains value"
+
+
+if False:
+	print "False"
+else:
+	print "True"
+
+val = 10
+
+if val == 10:
+	print "value is 10"
+elif val > 10:
+	print "value is > 10"
+elif val < 10:
+	print "value is lesser than 10"
+else:
+	print "value is unknow" 
