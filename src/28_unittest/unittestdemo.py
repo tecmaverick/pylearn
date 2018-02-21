cbca526a1af4b02036ef1cca03825b14658e5daf
AbrahamJP <abrahamjosephpeter@gmail.com>
2018-02-21 19:06:40 +1100

    added raise error unittest

--- src/28_unittest/unittestdemo.py
+++ src/28_unittest/unittestdemo.py
@@ -7,10 +7,18 @@ class Calc(unittest.TestCase):
 	def tearDown(self):
 		print "teardown"
 
+	def exceptionDemo(self):
+		raise ValueError
+
 	def getHello(self):
 		return "Hello"
 
 	def test_add(self):
 		print "Hello test"
 		self.assertEqual(self.getHello(),"Hello")
+
+	def test_raise_err(self):
+		with self.assertRaises(ValueError):
+			self.exceptionDemo()
+
 unittest.main()
