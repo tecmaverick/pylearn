4eff63f5c1d4dd3ac920ca403e8718204f96985d
AbrahamJP <abrahambiz001@gmail.com>
2023-01-24 22:56:10 +1100

    updated samples

--- src/43_algo/67_arr_sum.py
+++ src/43_algo/67_arr_sum.py
@@ -1,21 +1,29 @@
-
 def find_num1(n, lst):
-    sum_val = 0
     found = False
     result = []
     lst = sorted(lst, reverse=True)
 
-    for inner in lst:
-        result.append(inner)
-        sum_val += inner
+    while len(lst) > 1:
+        # reset
+        sum_val = 0
+        result = []
 
-        if sum_val == n:
-            found = True
+        for inner in lst:
+            result.append(inner)
+            sum_val += inner
+
+            if sum_val == n:
+                found = True
+                break
+
+            elif sum_val > n:
+                result.pop()
+                sum_val -= inner
+
+        if found:
             break
 
-        elif sum_val > n:
-            result.pop()
-            sum_val -= inner
+        lst.pop(0)
 
     if found:
         return result
@@ -23,10 +31,11 @@ def find_num1(n, lst):
         return []
 
 
-# a = [12, 11, 10, 5, 6, 3, 2, 1]
-a = [3, 4, 6, 1, 2, 9, 7, 5, 8]
+# a = [12, 11, 10, 5, 6, 3, 2]
+a = [ 2, 4, 6, 8, 10]
 # a = [12, 11, 10, 5, 6, 3, 2, 1, 4, 7, 8, 9, 13, 14, 15]
 
-for i in range(1, 50):
+# Find the elements in the array which when added up get the X value
+for i in range(1, sum(a)):
     result = find_num1(i, a)
     print("Num:{} Elements:{}".format(i, result))
